50174a8750b535a44236fbd240296cdcd96b0179
o-seoj <tjwjd010@naver.com>
2025-07-12 23:14:15 +0900

    add step5 Main01

---
+++ CodingTest/src/step5/Main01.java
@@ -0,0 +1,13 @@
+package step5;
+
+import java.util.Scanner;
+
+public class Main01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		int i = sc.nextInt();
+		System.out.println(s.charAt(i));
+	}
+}
