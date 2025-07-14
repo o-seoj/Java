b3dab1d5268b69e9a1d188b1f598591f4f769625
o-seoj <tjwjd010@naver.com>
2025-07-14 09:01:20 +0900

    add step6 main01

---
+++ CodingTest/src/step6/Main01.java
@@ -0,0 +1,21 @@
+package step6;
+/*
+문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+*/
+public class Main01 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\""+7);
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
