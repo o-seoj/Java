406f91ae1875e67e2e7aa341046d2f0269236381
o-seoj <tjwjd010@naver.com>
2025-07-23 16:54:20 +0900

    add test5

---
+++ JavaTest/src/test5/Test06.java
@@ -0,0 +1,47 @@
+package test5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+ * 날짜 : 2025/07/23
+ * 이름 : 오서정
+ * 내용 : 컬렉션 리스트 연습문제
+ */
+public class Test06 {
+	public static void main(String[] args) {
+		//60~100 사이 임의의 수 10개를 더하므로 결과값이 항상 다름
+		printList(createList());
+	}
+	
+	public static void printList(ArrayList<Integer> scoreList) {
+		int total = 0;
+		int size = scoreList.size();
+		
+		for(int i=0; i<size; i++) {
+			int score = scoreList.get(i);
+			total += score;
+			System.out.print(score);
+			
+			if(i==size-1) {
+				System.out.print(" = ");
+			}else {
+				System.out.print(" + ");
+			}
+		}
+		System.out.println(total);
+	}
+	
+	public static ArrayList createList() {
+		ArrayList<Integer> scoreList = new ArrayList<>();
+		Random rand = new Random();
+		
+		for(int i=1; i<=10; i++) {
+			//60~100 사이 임의의 수
+			int num = rand.nextInt(41) + 60;
+			scoreList.add(num);
+		}
+		return scoreList;
+	}
+}
