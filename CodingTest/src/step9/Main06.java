837b0d3150965bf230f34eaba9204cdbc4620cd3
o-seoj <tjwjd010@naver.com>
2025-07-24 18:25:57 +0900

    add step9

---
+++ CodingTest/src/step9/Main06.java
@@ -0,0 +1,16 @@
+package step9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main06 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		System.out.println(n);
+	}
+	
+}
