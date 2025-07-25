bdf319a8d0d8053a843e124b4bffa43c665a7365
o-seoj <tjwjd010@naver.com>
2025-07-25 10:01:06 +0900

    add step9 code

--- CodingTest/src/step9/Main06.java
+++ CodingTest/src/step9/Main06.java
@@ -3,6 +3,8 @@ package step9;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main06 {
 	public static void main(String[] args) throws IOException {
@@ -10,7 +12,28 @@ public class Main06 {
 		
 		String s = br.readLine();
 		int n = Integer.parseInt(s);
-		System.out.println(n);
+
+		for(int i=1; i<=n; i++) {
+			int count = 0;
+			//약수이면서 소수 구하기
+			if(n%i==0) {
+				for(int j =1; j<=i; j++) {
+					if(i%j==0) {
+						count++;
+					}
+				}
+				//약수이면서 소수 추가
+				if(count==2) {
+					while(true) {
+						if(n%i!=0) break;
+						if(n%i==0) {
+							System.out.println(i);
+							n = n/i;
+					}
+				}
+			}
+		}
+		}
 	}
 	
-}
+	}
